9435b84a05e5c0f3fcfe6bbf266f5f03530c140c
ReznichenkoKirill <Kirill.Reznichenkoo@gmail.com>
2020-12-05 10:25:10 +0200

    was changed Messages.php and Users.php

--- classes/Models/Users.php
+++ classes/Models/Users.php
@@ -3,5 +3,30 @@
 
 class Users
 {
+    protected $db;
 
+    public function __construct()
+    {
+        $this->db = new mysqli();
+    }
+
+    public function getAllUsers()
+    {
+        $query = "SELECT * FROM users;";
+        $result = $this->db->query($query);
+        return $result->fetch_all(MYSQLI_ASSOC);
+    }
+
+    public function getUser($login)
+    {
+        $query = "SELECT * FROM users WHERE login = $login;";
+        $result = $this->db->query($query);
+        return $result->fetch_assoc();
+    }
+
+    public function addUser($login)
+    {
+        $query = "INSERT INTO users (id, login) VALUES (NULL, '$login');";
+        $this->db->query($query);
+    }
 }
