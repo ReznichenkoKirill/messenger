cc7a92aa5ef163898de76a03911f97399c68ef58
ReznichenkoKirill <Kirill.Reznichenkoo@gmail.com>
2020-12-05 10:28:33 +0200

    was fix Messages.php

--- classes/Models/Messages.php
+++ classes/Models/Messages.php
@@ -12,14 +12,14 @@ class Messages
 
     public function getAllCorrespondence($recipient, $sender)
     {
-        $query = "SELECT message FROM messegas WHERE recipient_id = $recipient AND sender_id = $sender;";
+        $query = "SELECT message FROM messages WHERE recipient_id = $recipient AND sender_id = $sender;";
         $result = $this->db->query($query);
         return $result->fetch_all(MYSQLI_ASSOC);
     }
 
     public function addToCorrespondence($recipient, $sender, $mess)
     {
-        $query = "INSERT INTO messegas (id, message, sender_id, recipient_id) VALUES (NULL, '$mess' , '$sender', '$recipient');";
+        $query = "INSERT INTO messages (id, message, sender_id, recipient_id) VALUES (NULL, '$mess' , '$sender', '$recipient');";
         $this->db->query($query);
     }
 
