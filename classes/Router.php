5ce8018e795de9ac20ce70378e66d81689b90a5c
ReznichenkoKirill <Kirill.Reznichenkoo@gmail.com>
2020-12-05 09:42:11 +0200

    was changed Router.php, View.php and Controllers Api.php, error404.php

--- classes/Router.php
+++ classes/Router.php
@@ -1,7 +1,38 @@
 <?php
 
+use Controllers\error404;
 
 class Router
 {
+    static public function init()
+    {
+        session_start();
+        $uri = $_SERVER['REQUEST_URI'];
+        $uriArray = explode('/', $uri);
+        array_shift($uriArray);
+        $controllerName = (!empty($uriArray[0])) ? mb_strtolower($uriArray[0]) : 'index';
+        $action = (!empty($uriArray[1])) ? mb_strtolower($uriArray[1]) : 'index';
+        $controllerClass = '\Controllers\\' . ucfirst($controllerName);
+        if (!class_exists($controllerClass)) {
+            self::error_404();
+        }
+        $controller = new $controllerClass;
+        if (!method_exists($controller, $action)) {
+            self::error_404();
+        }
+        $controller->$action();
+    }
 
+    static public function error_404()
+    {
+        http_response_code(404);
+        $controller = new error404();
+        $controller->index();
+        exit();
+    }
+
+    static public function redirect($url)
+    {
+        header('Location: ' . $url);
+    }
 }
--- classes/View.php
+++ classes/View.php
@@ -3,5 +3,12 @@
 
 class View
 {
+    public $template;
+    public $page;
 
+    public function render()
+    {
+        $this->page = str_replace('.', DIRECTORY_SEPARATOR, $this->page);
+        include_once 'views' . DIRECTORY_SEPARATOR . $this->template . '.php';
+    }
 }
